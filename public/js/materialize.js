0a557c4f62a4b442f44994df3719ce759e72b86c
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-01-16 17:51:22 -0500

    Added CSS

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -2,16 +2,15 @@
 $(document).ready(function() {
   $(".sidenav").sidenav();
   $(".modal").modal();
+});
 
-  //Changes Background and Carpet Colors
-  $(".rug").click(function() {
-    $(".background-wall").css({
-      transition: "background 1s ease",
-      background: "lightblue"
-    });
-    $("body").css({
-      transition: "background 1.4s ease",
-      background: "#de5164"
-    });
+$(".rug").click(function() {
+  $(".background-wall").css({
+    transition: "background 1s ease",
+    background: "lightblue"
+  });
+  $("body").css({
+    transition: "background 1.4s ease",
+    background: "#de5164"
   });
 });
