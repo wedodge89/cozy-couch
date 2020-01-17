31cdf89fcbf65c5f602af94fa13a11778c537332
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-01-16 20:11:41 -0500

    Added scaling

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -4,34 +4,13 @@ $(document).ready(function() {
   $(".modal").modal();
 });
 
-function invert() {
-  $(".rug").click(function() {
-    $(".background-wall").css({
-      transition: "background 1s ease",
-      background: "lightblue"
-    });
-    $("body").css({
-      transition: "background 1.4s ease",
-      background: "#de5164"
-    });
+$(".rug").click(function() {
+  $(".background-wall").css({
+    transition: "background 1s ease",
+    background: "lightblue"
   });
-}
-
-function unInvert() {
-  $(".rug").click(function() {
-    $(".background-wall").css({
-      transition: "background 1s ease",
-      background: "#de5164"
-    });
-    $("body").css({
-      transition: "background 1.4s ease",
-      background: "lightblue"
-    });
+  $(".carpet").css({
+    transition: "background 1.4s ease",
+    background: "#de5164"
   });
-}
-
-if (document.body.background == "#add8e6") {
-  invert();
-} else {
-  unInvert();
-}
+});
