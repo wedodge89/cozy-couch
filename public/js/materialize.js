dd881245b5c6134eb0622e1a7bbef0b2e7875dcb
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-01-16 19:26:56 -0500

    Added CSS and floor div

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -4,13 +4,34 @@ $(document).ready(function() {
   $(".modal").modal();
 });
 
-$(".rug").click(function() {
-  $(".background-wall").css({
-    transition: "background 1s ease",
-    background: "lightblue"
+function invert() {
+  $(".rug").click(function() {
+    $(".background-wall").css({
+      transition: "background 1s ease",
+      background: "lightblue"
+    });
+    $("body").css({
+      transition: "background 1.4s ease",
+      background: "#de5164"
+    });
   });
-  $("body").css({
-    transition: "background 1.4s ease",
-    background: "#de5164"
+}
+
+function unInvert() {
+  $(".rug").click(function() {
+    $(".background-wall").css({
+      transition: "background 1s ease",
+      background: "#de5164"
+    });
+    $("body").css({
+      transition: "background 1.4s ease",
+      background: "lightblue"
+    });
   });
-});
+}
+
+if (document.body.background == "#add8e6") {
+  invert();
+} else {
+  unInvert();
+}
