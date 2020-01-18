b30afb8dbb5d3d33532b429cbccfe40aeea3ed67
Jeffrey Poe <jeffreypoe@Jeffreys-MacBook-Pro-2.local>
2020-01-18 10:02:37 -0500

    Added color inversion

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -4,17 +4,35 @@ $(document).ready(function() {
   $(".modal").modal();
 });
 
-$(".rug").click(function() {
-  $("#background-wall").css({
-    transition: "background 1s ease",
-    background: "lightblue"
-  });
-  $(".carpet").css({
-    transition: "background 1.4s ease",
-    background: "#de5164"
-  });
-  $(".iwantthiscolor").css({
-    transition: "background 1.4s ease",
-    background: "#de5164"
-  });
+//Console Logs if the button has been pressed
+//Runs Invert Colors
+$("#invert-colors").click(function() {
+  console.log(document.getElementById("invert-colors").checked);
+  if (document.getElementById("invert-colors").checked !== true) {
+    $("#background-wall").css({
+      transition: "background 1s ease",
+      background: "lightblue"
+    });
+    $(".carpet").css({
+      transition: "background 1.4s ease",
+      background: "#de5164"
+    });
+    $(".iwantthiscolor").css({
+      transition: "background 1.4s ease",
+      background: "#de5164"
+    });
+  } else if (document.getElementById("invert-colors").checked === true) {
+    $("#background-wall").css({
+      transition: "background 1s ease",
+      background: "#de5164"
+    });
+    $(".carpet").css({
+      transition: "background 1.4s ease",
+      background: "lightblue"
+    });
+    $(".iwantthiscolor").css({
+      transition: "background 1.4s ease",
+      background: "lightblue"
+    });
+  }
 });
