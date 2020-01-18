9fc28dfde5147dd8299c027e392bc33ff7b02584
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-01-18 10:58:43 -0500

    finished basic user auth

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,9 +9,14 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/register", function(req, res) {
+    db.User.create(req.body).then(function(data) {
+      res.json(
+        {
+          id: data.id, 
+          firstName: data.firstName
+        }
+      );
     });
   });
 
