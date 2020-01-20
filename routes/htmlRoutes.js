ed6e5ab6db5969c33d257a194ae155f23accfc7f
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-01-20 17:45:22 -0500

    html routes done

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,32 +1,20 @@
 var db = require("../models");
 var path = require("path");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+  module.exports = function(app) {
+      app.get("/", function(req, res) {
+          res.sendFile(path.join(__dirname, "../public/index.html"))
       });
-    });
-  });
-
-  app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/register.html"))
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  
+      app.get("/register", function(req, res) {
+          res.sendFile(path.join(__dirname, "../public/register.html"));
       });
+  
+      app.get("/login", function(req, res) {
+          res.sendFile(path.join(__dirname, "../public/login.html"))
+      });
+  
+      app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
     });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
+  };
