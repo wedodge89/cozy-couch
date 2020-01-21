54c4b9555c33e04292bc197177065b24e1c4cc83
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-01-20 22:38:07 -0500

    working on login api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,12 +22,25 @@ module.exports = function(app) {
 
   // Log-in existing user
   app.post("/api/login", function(req, res) {
-    db.User.create(req.body).then(function(data) {
+    db.User.findAll({
+      where: {
+        email: req.body.email
+      }
+    }).then(function(data) {
       res.json(
         {
-          id: data.id, 
-          firstName: data.firstName
+          data: data
         }
-      );
-    });
+      )
+        // console.log(data)
+        console.log(`DB Password: ${data[0].dataValues.pw}`)
+        console.log(`Should be input password, but isn't: ${req.body.password}`)
+        if (data[0].dataValues.pw === req.body.password) {
+          localStorage.clear();
+          localStorage.setItem("id", data.id)
+          localStorage.setItem("firstName", data.firstName)
+        }
+    })
   });
+
+};
