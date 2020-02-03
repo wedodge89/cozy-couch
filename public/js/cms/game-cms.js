0a0996059e6ec4c1933a4579ef1f5759c895e07a
Jeffrey Poe <jeffreypoe@jeffreys-mbp-2.lan>
2020-02-03 15:30:49 -0500

    Fixed double get request error

--- public/js/cms/game-cms.js
+++ public/js/cms/game-cms.js
@@ -13,16 +13,13 @@ $("#game-cms").on("submit", function(event) {
   $.post("/api/games", Game).then(function(data) {
     console.log(data);
 
-    $.get("/api/games", function(data) {
-      console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        let row = $("<tr>");
-        let title = $("<td>").text(data[i].gameTitle);
-        let link = $("<td>").text(data[i].gameLink);
-        row.append(title, link);
-        $("#your-games").append(row);
-      }
-    });
+    console.log(data);
+    let row = $("<tr>");
+    let title = $("<td>").text(Game.gameTitle);
+    let link = $("<td>").text(Game.gameLink);
+    row.append(title, link);
+    $("#your-games").append(row);
+
     $("#game-title").val("");
     $("#game-body").val("");
   });
